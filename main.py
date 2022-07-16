dffd1406110430b33921cb7240e7f9ffe8c8da47
Jonas Serafim <serajonas@gmail.com>
2022-07-16 12:48:00 -0300

    MVC

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-usuario = input("Informe o seu Usuário > ")
-senha = input("Informe a sua Senha > ")
+# View - o que vai para o usuário
+def view():
+    usuario_digitado = input("Informe o seu usuário: ")
+    senha_digitado = input("Informe sua senha: ")
 
-if usuario  == 'joao' and senha == '123':
-    print("*** Pode entrar *** ")
-else:
-    print(" *** Usuario ou senha inválidos ***")
+# Model - O que vem do banco de dados (BD)
+def model():
+    usuario_BD = 'joao'
+    senha_BD = '123'
+
+# Controller - a validação
+def controller():
+    if usuario_digitado == usuario_BD and senha_digitado == senha_BD:
+        print("pode entrar")
+    else:
+        print("usuário ou senha inválido")
