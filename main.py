0132df16f3d37b9b77ef98399ff030be770a1339
Jonas Serafim <serajonas@gmail.com>
2022-07-16 11:38:49 -0300

    User

---
+++ main.py
@@ -0,0 +1,7 @@
+usuario = input("Informe o seu Usuário ")
+senha = input("Informe a sua Senha")
+
+if usuario  == 'joao' and senha == '123' ;
+    print("pode entrar")
+else:
+    print("usuario ou senha invalidos")
