17372e98f82506535e12888b8e4d76e83427935b
Jonas Serafim <serajonas@gmail.com>
2022-07-16 13:52:51 -0300

    Final de Hoje

--- main.py
+++ main.py
@@ -1,27 +1,3 @@
-# View - o que vai para o usuário
-def view():
-    usuario_digitado = input("Informe o seu usuário: ")
-    senha_digitado = input("Informe sua senha: ")
-    controller(usuario_digitado,senha_digitado)
-    
+import formulario
 
-# Model - O que vem do banco de dados (BD)
-def model_usuario():
-    usuario_BD = 'joao'
-    return usuario_BD
-def model_senha():    
-    senha_BD = '123'
-    return senha_BD
-
-
-# Controller - a validação
-def controller(usuario_digitado,senha_digitado):
-    usuario_BD = model_usuario()
-    senha_BD = model_senha()
-
-    if usuario_digitado == usuario_BD and senha_digitado == senha_BD:
-        print("pode entrar")
-    else:
-        print("usuário ou senha inválido")
-
-view()        
+formulario.formulario_login()
---
+++ validacao.py
@@ -0,0 +1,10 @@
+import banco
+# Controller - a validação
+def validar_login(usuario_digitado,senha_digitado):
+    usuario_BD = banco.model_usuario()
+    senha_BD = banco.model_senha()
+
+    if usuario_digitado == usuario_BD and senha_digitado == senha_BD:
+        print("pode entrar")
+    else:
+        print("usuário ou senha inválido")
