b1a692c132b995663318b9978b6d9f416a445f25
Jonas Serafim <104076718+serajonas@users.noreply.github.com>
2022-07-23 11:18:54 -0300

    Alterado os BD dados

--- models/banco.py
+++ models/banco.py
@@ -1,9 +1,14 @@
-
-
 # Model - O que vem do banco de dados (BD)
 def model_usuario():
-    usuario_BD = 'joao'
+    arquivo = open("models\\dados.txt","r+")
+    conteudo = arquivo.readlines()
+    for linha in conteudo:
+        usuario_senha = linha.split(";")
+    usuario_BD = usuario_senha[0]
     return usuario_BD
-def model_senha():    
+
+def model_senha():
     senha_BD = '123'
     return senha_BD
+
+print(model_usuario())
