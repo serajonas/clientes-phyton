a958561f103b36e7e543c582b2909a5a16c79904
Jonas Serafim <104076718+serajonas@users.noreply.github.com>
2022-07-23 12:01:08 -0300

    Dados Alterados

--- models/banco.py
+++ models/banco.py
@@ -1,6 +1,6 @@
 # Model - O que vem do banco de dados (BD)
 def model_usuario():
-    arquivo = open("models\\dados.txt","r+")
+    arquivo = open("models\\usuarios.txt","r+")
     conteudo = arquivo.readlines()
     for linha in conteudo:
         usuario_senha = linha.split(";")
@@ -8,7 +8,10 @@ def model_usuario():
     return usuario_BD
 
 def model_senha():
-    senha_BD = '123'
-    return senha_BD
+    arquivo = open("models\\usuarios.txt","r+")
+    conteudo = arquivo.readlines()
+    for linha in conteudo:
+        usuario_senha = linha.split(";")
+    usuario_BD = usuario_senha[1]
+    return usuario_BD
 
-print(model_usuario())
--- views/formulario.py
+++ views/formulario.py
@@ -1,6 +1,6 @@
-import controllers.validacao as validacao
+import controllers.controllerAplicacao as controllerAplicacao
 # View - o que vai para o usuário
 def formulario_login():
     usuario_digitado = input("Informe o seu usuário: ")
     senha_digitado = input("Informe sua senha: ")
-    validacao.validar_login(usuario_digitado,senha_digitado)
+    controllerAplicacao.validar_login(usuario_digitado,senha_digitado)
